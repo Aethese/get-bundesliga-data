8031933f8462b0c838d147ba3b07c1a112bdeea1
Aethese <59108437+Aethese@users.noreply.github.com>
2023-09-12 10:16:39 -0500

    clean up backend, and get_data skeleton updated
    can now call the function from main HTML page, just need to get the API called and data collected
    
    Co-Authored-By: imKeiki <95663041+imkeiki@users.noreply.github.com>

--- get_data.js
+++ get_data.js
@@ -2,10 +2,28 @@
 let YEAR = '2023'
 let API_URL = 'https://api.openligadb.de/getmatchdata/bl1/' + YEAR
 
-function getData(team, game) {
+function updateTable(team, game) {
 	/**
+	 * @description updates the table. gets the data from getData()
+	 * 
 	 * @param {String}  team The team we're looking up
 	 * @param {Integer} game What game we're going to look up
 	 */
+	let data = getData(team, game);
+	let table = document.getElementById('dataTable');
+
+	return 0;
+};
+
+function getData(team, game) {
+	/**
+	 * @description gets all of the data needed in updateTable()
+	 * 
+	 * @param {String}  team The team we're looking up
+	 * @param {Integer} game What game we're going to look up
+	 * 
+	 * @returns {Dictionary} data full data that we're working with
+	 */
+
 	return 0;
 };
