674e6773e41403fbd803a4fcec43c7fb06ce916e
Aethese <59108437+Aethese@users.noreply.github.com>
2023-09-12 09:36:31 -0500

    alphabetize list and get_data skeleton

--- get_data.js
+++ get_data.js
@@ -1,3 +1,11 @@
 // https://api.openligadb.de/getmatchdata/bl1/2023/1
 let YEAR = '2023'
 let API_URL = 'https://api.openligadb.de/getmatchdata/bl1/' + YEAR
+
+function getData(team, game) {
+	/**
+	 * @param {String}  team The team we're looking up
+	 * @param {Integer} game What game we're going to look up
+	 */
+	return 0;
+};
