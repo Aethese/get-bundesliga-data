69236b617320c34e9ff9005ea786b990fd2a25a8
Aethese <59108437+Aethese@users.noreply.github.com>
2023-09-14 13:51:18 -0500

    get data from API
    actually get the data from the API. not processing it yet or applying the data to the table, but that's a work in progress :)
    
    Co-Authored-By: imKeiki <95663041+imkeiki@users.noreply.github.com>

--- get_data.js
+++ get_data.js
@@ -1,15 +1,35 @@
-// https://api.openligadb.de/getmatchdata/bl1/2023/1
 let YEAR = '2023'
 let API_URL = 'https://api.openligadb.de/getmatchdata/bl1/' + YEAR
 
-function updateTable(team, game) {
+async function callAPI(team)
+{
+	/**
+	 * @description gets all of the data needed in updateTable()
+	 * 
+	 * @param {String}  team The team we're looking up
+	 * 
+	 * @returns {Array} data full data that we're working with
+	 */
+
+	let URL = API_URL + '/' + team;
+	const response = await fetch(URL, {
+		headers: {
+			'Accept': 'application/json'
+		}
+	});
+	let data = await response.json();
+	return data;
+}
+
+async function updateTable(team, game)
+{
 	/**
 	 * @description updates the table. gets the data from getData()
 	 * 
 	 * @param {String}  team The team we're looking up
 	 * @param {Integer} game What game we're going to look up
 	 */
-	let data = getData(team, game);
+	let data = await callAPI(team);
 	let table = document.getElementById('dataTable');
 	table.classList.remove('hidden');
 
@@ -24,23 +44,46 @@ function updateTable(team, game) {
 	let diff      = document.getElementById('diff');
 	let punkte    = document.getElementById('punkte');
 	let platz     = document.getElementById('platz');
-	let itemList = [
+	let itemList  = [
 		spielt, datum, gegner, eregebnis,
 		guv, tore, diff, punkte, platz
 	];
 
-	return 0;
-};
+	for (let i = 0; i <= itemList.length; i++)
+	{
+		//
+	};
 
-function getData(team, game) {
+	return 0;
+}
+
+function getData(team)
+{
 	/**
 	 * @description gets all of the data needed in updateTable()
 	 * 
 	 * @param {String}  team The team we're looking up
 	 * @param {Integer} game What game we're going to look up
 	 * 
-	 * @returns {Dictionary} data full data that we're working with
+	 * @returns {Array} data full data that we're working with
 	 */
-	let fullData = API_URL;
-	return 0;
-};
+	let URL = API_URL + '/' + team;
+	let jsonData;
+
+	fetch(URL, {
+		headers: {
+			'Accept': 'application/json'
+		}
+	}).then(response => response.json())
+	  .then(data => {
+		jsonData = data;
+	});
+	return jsonData;
+	/* end here
+	if (data[game-1]['team1']['teamName'] === team)
+	{
+		console.log('Our team')
+	} else {
+		console.log('Not our team')
+	}*/
+}
