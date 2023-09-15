f0473de8dbaf524a5c06dc7bf08c5adf7bf874a4
Aethese <59108437+Aethese@users.noreply.github.com>
2023-09-15 14:24:05 -0500

    get date, team name, and score
    can't get anymore data at this time unless i use another API service. comment on line 60 explains this issue better. will open a issue to address this issue as well

--- get_data.js
+++ get_data.js
@@ -1,24 +1,71 @@
 let YEAR = '2023'
 let API_URL = 'https://api.openligadb.de/getmatchdata/bl1/' + YEAR
 
-async function callAPI(team)
+async function getTeamData(team)
+{
+	/**
+	 * @descriptions gets data about a specific team
+	 * 
+	 * @param {String} team The team we're looking up
+	 * 
+	 * @returns {Array} Array full of data we need
+	 */
+
+	return 0;
+}
+
+async function getData(team, game)
 {
 	/**
 	 * @description gets all of the data needed in updateTable()
 	 * 
-	 * @param {String}  team The team we're looking up
+	 * @param {String}  team  The team we're looking up
+	 * @param {Integer} game  What game we're going to look up
 	 * 
 	 * @returns {Array} data full data that we're working with
 	 */
 
-	let URL = API_URL + '/' + team;
+	const URL = API_URL + '/' + team;
 	const response = await fetch(URL, {
 		headers: {
 			'Accept': 'application/json'
 		}
 	});
 	let data = await response.json();
-	return data;
+
+	// order is the same as it is on the table
+
+	let date = data[game-1]['matchDateTime'].substring(0, 10);
+
+	let team1 = true; // if we're team 1
+	let Team = data[game-1]['team1']['shortName'];
+	if (Team !== team) // if first team is diff then what we're looking for
+	{
+		team1 = false;
+		Team = data[game-1]['team2']['shortName'];
+	}
+
+	let matchResults = data[game-1]['matchResults'][1];
+	let score;
+	let score1 = matchResults['pointsTeam1'];
+	let score2 = matchResults['pointsTeam2'];
+	if (team1) // if we're team 1
+	{
+		score = score1+'-'+score2;
+	} else {
+		score = score2+'-'+score1;
+	}
+
+	/*
+	The rest of the data we need to gather can't be gathered
+	from openligadb.de i need a new source to get specific
+	info on teams from
+	*/
+
+	finalInfo = [
+		game, date, Team, score
+	];
+	return finalInfo;
 }
 
 async function updateTable(team, game)
@@ -26,10 +73,11 @@ async function updateTable(team, game)
 	/**
 	 * @description updates the table. gets the data from getData()
 	 * 
-	 * @param {String}  team The team we're looking up
-	 * @param {Integer} game What game we're going to look up
+	 * @param {String}  team  The team we're looking up
+	 * @param {Integer} game  What game we're going to look up
 	 */
-	let data = await callAPI(team);
+	let data = await getData(team, game);
+	console.log(data);
 	let table = document.getElementById('dataTable');
 	table.classList.remove('hidden');
 
